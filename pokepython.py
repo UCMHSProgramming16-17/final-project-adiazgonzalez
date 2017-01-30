5eebe8b203e28968572305f1e53531e7be4ba3c7
Andrea Diaz Gonzalez <adiazgonzalez@ucvts.org>
2017-01-30 17:00:49 +0000

    final final project, since I forgot to commit

--- pokepython.py
+++ pokepython.py
@@ -4,14 +4,19 @@ import pandas as pd
 
 df = pd.read_csv('moves.csv')
 
-from bokeh.charts import Bar, output_file, save
+from bokeh.charts import Bar, Scatter, output_file, save
 
 
 df =df[df['power'] != '—']
 
 print(df)
 
-pokebar= Bar(df, 'type', values='power', agg = 'count', title= "Amount of Moves per Type")
+pokebar= Bar(df, 'type', values='power', agg = 'count', title= "Number of Moves per Type")
+
+pokescatter= Scatter(df, x= "pp", y="power", title= "Power Relative to PP of Pokemon Moves")
 
 output_file('pokebar.html')
 save(pokebar)
+
+output_file('pokescatter.html')
+save(pokescatter)
